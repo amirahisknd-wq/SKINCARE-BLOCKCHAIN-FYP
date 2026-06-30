c6532b5cd46a2d394770f9e33e13c47e9b5a37b9
Amirah <amirahisknd@gmail.com>
2026-06-30 13:33:42 +0800

    Edit Login Pages

--- Manufacturer/src/pages/LoginPage.js
+++ Manufacturer/src/pages/LoginPage.js
@@ -16,6 +16,8 @@ function LoginPage() {
 
     try {
 
+     console.log(process.env.REACT_APP_API_URL); 
+
       const response =
         await axios.post(
           `${process.env.REACT_APP_API_URL}/login`,
